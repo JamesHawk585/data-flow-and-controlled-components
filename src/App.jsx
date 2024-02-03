137ac4989c7e9e6861d07613ec089d4c00ae566e
James Hawk <jph94880@gmail.com>
2024-02-03 17:08:37 -0600

    Adds form to LoginForm.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LoginForm from './LoginForm';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+
+  return (
+<main>
+  <LoginForm/>
+</main>
+  );
+}
+
+export default App;
---
+++ src/LoginForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState }from 'react'
+
+
+const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("")
+
+    const handleUserNameSubmit = (e) => {
+        setUsername(e.target.value)
+    } 
+
+    const handlePasswordSubmit = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        console.log(e)
+    }
+
+    // 2023_08_16_Controlled_Components 40:02
+
+  return (
+    <form onSubmit={(e) => handleFormSubmit(e)}>
+        <input type='text' id= 'passwordInput' placeholder="Username" value={username} onChange={(e) => handleUserNameSubmit(e)}></input>
+        <input type='password' id='usernameInput' placeholder='Password' value={password} onChange={(e) => handlePasswordSubmit(e)}></input>
+        <button id ='submitButton'>Submit</button>
+    </form>
+  )
+}
+
+export default LoginForm
