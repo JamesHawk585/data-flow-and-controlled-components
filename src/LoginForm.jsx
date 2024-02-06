f76d02f23f22424a30266ff9b41eacd9851b20cb
James Hawk <jph94880@gmail.com>
2024-02-05 18:14:33 -0600

    Password rules works correctly.

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -4,24 +4,54 @@ const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const passwordString = password;
+
+  //   regex (js Regular Expressions are used to check if a string meets given criteria.)
+  const passwordRules = {
+    "Long Enough": (passwordString) => passwordString.length >= 8,
+    "Has a Number": (passWordContainsNumber) => /\d/.test(passwordString),
+    "Special Character(!;.$)": (passwordString) =>
+      /[!;.$]/.test(passwordString),
+    "Uppercase Letter": (passwordString) =>
+      passwordString.toLowerCase() !== passwordString,
+  };
+
+  console.log(Object.entries(passwordRules))
+
+
+
+//   Ternary syntax: 
+// question yes : no
+// ? "": ""
+
+  const passwordRulesElements = Object.entries(passwordRules).map(([str, func]) => <li key='str'>{func(password) ? "✅": "❌"}    {str}</li>)
+
   const handleUserNameSubmit = (e) => {
     setUsername(e.target.value);
+    console.log("e.target.value: ", e.target.value);
+    console.log("username: ", username);
   };
 
   const handlePasswordSubmit = (e) => {
     setPassword(e.target.value);
+    console.log("password:", password);
   };
 
-  const handleFormSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    console.log(e);
+    const userdataObject = {
+      username,
+      password,
+    };
+    console.log(userdataObject);
   };
 
   // 2023_08_16_Controlled_Components 40:02
 
   return (
-    <form onSubmit={(e) => handleFormSubmit(e)} id='loginForm'>
-      <label id='loginLabel'>Login:</label>
+    <>
+      <form onSubmit={(e) => handleLogin(e)} className="loginForm">
+        <label id="loginLabel">Login:</label>
         <input
           type="text"
           id="passwordInput"
@@ -36,8 +66,16 @@ const LoginForm = () => {
           value={password}
           onChange={(e) => handlePasswordSubmit(e)}
         ></input>
-      <button id="submitButton">Submit</button>
-    </form>
+        <button id="submitButton">Submit</button>
+      </form>
+      <section>
+        <header>
+          <div>
+            <ul>{passwordRulesElements}</ul>
+          </div>
+        </header>
+      </section>
+    </>
   );
 };
 
